28adb11df801b479142d980baa672605ba30a43c
danilads <vfhn3fghtkm>
2018-10-16 10:25:28 +0300

    update

--- components/Block_MainPage.js
+++ components/Block_MainPage.js
@@ -1,5 +1,4 @@
 import React,{Fragment} from 'react';
-import Filter from '../primitives/Filter';
 import './Block_MainPage.scss';
 class Block_MainPage extends React.PureComponent {
 	
@@ -8,26 +7,37 @@ class Block_MainPage extends React.PureComponent {
 	};
 
   	render() {
-		  console.log('hi');
 		return (
 			<Fragment>
 			
 			<div className="container">
 				<div className="row">
-					<div className="col-1 cl1"><div>1</div></div>
-					<div className="col-1 cl1">2</div>
-					<div className="col-1 cl1">3</div>
-					<div className="col-1 cl1">4</div>
-					<div className="col-1 cl1">5</div>
-					<div className="col-1 cl1">6</div>
-					<div className="col-1 cl1">7</div>
-					<div className="col-1 cl1">8</div>
-					<div className="col-1 cl1">9</div>
-					<div className="col-1 cl1">10</div>
-					<div className="col-1 cl1">11</div>
-					<div className="col-1 cl1">12</div>
-
-					<div className="col-12"><div className="cl1">12</div></div>
+					<div className="col-12"><div className="cl1">desktop</div></div>
+				</div>
+				<div className="row">
+					<div className="col-1"><div className="cl1">1</div></div>
+					<div className="col-1"><div className="cl1">2</div></div>
+					<div className="col-1"><div className="cl1">3</div></div>
+					<div className="col-1"><div className="cl1">4</div></div>
+					<div className="col-1"><div className="cl1">5</div></div>
+					<div className="col-1"><div className="cl1">6</div></div>
+					<div className="col-1"><div className="cl1">7</div></div>
+					<div className="col-1"><div className="cl1">8</div></div>
+					<div className="col-1"><div className="cl1">9</div></div>
+					<div className="col-1"><div className="cl1">10</div></div>
+					<div className="col-1"><div className="cl1">11</div></div>
+					<div className="col-1"><div className="cl1">12</div></div>
+				</div>
+				<div className="row">
+					<div className="col-12"><div className="cl1">mobile</div></div>
+				</div>
+				<div className="row">
+					<div className="col-2"><div className="cl1">1</div></div>
+					<div className="col-2"><div className="cl1">2</div></div>
+					<div className="col-2"><div className="cl1">3</div></div>
+					<div className="col-2"><div className="cl1">4</div></div>
+					<div className="col-2"><div className="cl1">5</div></div>
+					<div className="col-2"><div className="cl1">6</div></div>
 				</div>
 			</div>
 			</Fragment>
