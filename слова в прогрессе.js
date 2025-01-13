e7170c8f5f4f2b4472f27a170b0258f710210413
Daniil Shemetau <daniil@daniils-air.home>
2025-01-13 17:56:44 +0100

    upd

---
+++ слова в прогрессе.js
@@ -0,0 +1,185 @@
+const WORDS1 = [
+	['bar', 'бар'],
+	[безобразие – skandal]
+[sierota, сирота ]
+[беспредел swawola, anarchia]
+[ближайший najbliższy]
+[больной chory]
+[большинство – większość]
+[вернуть – zwrócić, oddać]
+[вернуться – wrócić, powrócić]
+[весь – cały]
+[весьма – całkiem, dosyć]
+[вечеринка – impreza]
+[вещь – rzecz]
+[взломщик – włamywacz
+вид – widok
+включая – wraz
+вовсе – całkiem
+возвращать – zwracać
+возвращаться – powracać
+вообще – w ogóle, wcale
+запрещать – zabraniać
+вроде – niby
+всё-таки – jednak
+втягивать – wciągać
+выезжать – wyjeżdżać
+выжить – wytrzymać,
+высокоточный – precyzyjny
+выставка – wystawa
+гадить – brudzić, paskudzić
+газон – trawnik
+горничная – pokojówka
+данные – dane
+движение – ruch
+действительно – naprawdę
+джинсы – jeansy
+диапазон – zakres
+диплом (разг.) –  magisterka
+доблестный – bohaterski, odważny, waleczny
+доблесть – bohaterstwo, odwaga, waleczność
+доброволец – ochotnik
+дожить – dożyć
+достопримечательность –osobliwość
+дырка – dziura
+желать – życzyć
+желая – chcąc
+житель – mieszkaniec
+заканчивать – kończyć
+замечайтельный – znakomity, wspaniały
+заполучить – dostać, zdobyć
+заседание – posiedzenie
+значение – wartość
+издательство – wydawnictwo
+изделие – wyrób, produkt
+издержки – koszty
+изменить – zmienić
+изучать – studiować
+именинник – solenizant, jubilat
+инсценировка – mistyfikacja
+карманник – kieszonkowiec
+качели – huśtawka
+квартал – dzielnica
+ковёр – dywan
+кое-что – coś
+кондитерская – cukiernia
+конкретно – konkretnie
+конь – koń
+костёл – kościół
+кукла – kukła
+кулёк – siatka, reklamówka
+культура – uprawa
+кустарник – krzew
+ледник – lodowiec
+лектор – lektor
+лекция – wykład
+лишь – tylko
+любезный – uprzejmy
+мелочь – drobnostka, drobne
+меньшинство – mniejszość
+мороженое – lody
+мостовая – jezdnia
+мотороллер – skuter
+мошенник – oszust
+мчаться – mknąć, pędzić
+начало – początek
+начинающий – początkujący
+недействительный – nieważny
+неоновый – neonowy
+объяснить – wyjaśnić
+обычно – na ogół, zazwyczaj
+обязательно – koniecznie
+овощи – warzywa
+огни – światła
+олень – jeleń, renifer
+опять – znowu
+особенно – szczególnie
+ответ – odpowiedź
+ответить – odpisać
+отделение – oddział
+отменить – odwołać
+отпуск – urlop
+отметить – zaznaczyć
+охотник – myśliwy
+памятник – pomnik
+передать – przekazać
+пережить – przeżyć, przetrwać
+перекрёсток – skrzyżowanie
+периметр – obwód
+поганый – parszywy
+подальше – dalej 
+подойти – podejść, nadejść,
+подробно – szczegółowo
+подходить – odpowiadać, pasować
+подъезжать – podjeżdżać
+подъезд – podjazd, klatka 
+позволить – pozwolić
+попытка – próba
+постоянно – ciągle
+посчитать – policzyć
+потерять – zgubić, stracić
+правда – rzeczywiście
+предупредить – uprzedzić
+преподаватель – wykładowca
+преподавать – wykładać
+пригород – przedmieście
+придумать – wymyślić
+пробник – próbka
+проводить – spędzać (czas), odprowadzić
+производство – wykonanie
+промышленный – przemysłowy
+прорыв – przełom
+проснуться – obudzić 
+прочный – trwały, masywny, mocny, wytrzymały
+размышлять – rozmyślać
+разноцветный – różnokolorowy
+разрешить – pozwolić
+решить – zdecydować, postanowić, rozwiązać (zadanie)
+свернуть – zwinąć, skręcić
+сдaча – reszta
+сдать – zdać, oddać
+сесть – usiąść
+сидеть – siedzieć
+скамейка – ławka
+слать – słać, wysyłać
+слитно – łącznie
+слишком – za, zbyt
+смета – kosztorys
+снова – znowu
+совсем – całkiem
+спасать – ratować
+спешить – spieszyć, 
+спичка – zapałka
+стая – ławica
+столовка – stołówka
+строго – surowo, ściśle
+сумерки – zmrok
+счёт – rachunek
+считать – liczyć
+туалет – toaleta
+увлекать – wciągać
+уезжать – odjeżdżać, wyjeżdżać
+узнать – dowiedzieć
+уснуть – usnąć, zasnąć
+успеть – zdążyć
+устарелый – nieaktualny
+фрукты – owoce
+хотя – chociaż
+целый – cały
+часто – często
+Поезд - pociąg
+ярко – jasno, jaskrawo
+
+Proszę - Пожалуйста
+
+Żegnaj - прощай
+Przepraszam- извнините
+wybaczać - извинять
+извиняться - przepraszać
+Конечно - Oczywiście
+Согласен - Zgoda
+Dosyć - Хватит
+Chłopczyku - мальчик
+Dziewczynko - девочка
+
+]
